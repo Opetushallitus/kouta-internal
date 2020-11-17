ea2fdc4c67aebc8dbc76dbfe4dd45e8b10df5585
August Kilponen <august.kilponen@gmail.com>
2020-11-17 16:55:18 +0200

    Tuki ammatillisen koulutuksen tutkinnon osille

--- src/main/scala/fi/oph/kouta/internal/domain/enums/Koulutustyyppi.scala
+++ src/main/scala/fi/oph/kouta/internal/domain/enums/Koulutustyyppi.scala
@@ -4,11 +4,12 @@ sealed abstract class Koulutustyyppi(val name: String) extends BasicType
 
 object Koulutustyyppi extends BasicTypeCompanion[Koulutustyyppi] {
 
-  case object Amm extends Koulutustyyppi("amm")
-  case object Lk  extends Koulutustyyppi("lk")
-  case object Muu extends Koulutustyyppi("muu")
-  case object Yo  extends Koulutustyyppi("yo")
-  case object Amk extends Koulutustyyppi("amk")
+  case object Amm             extends Koulutustyyppi("amm")
+  case object AmmTutkinnonOsa extends Koulutustyyppi("amm-tutkinnon-osa")
+  case object Lk              extends Koulutustyyppi("lk")
+  case object Muu             extends Koulutustyyppi("muu")
+  case object Yo              extends Koulutustyyppi("yo")
+  case object Amk             extends Koulutustyyppi("amk")
 
-  val all: List[Koulutustyyppi] = List(Amm, Lk, Muu, Yo, Amk)
+  val all: List[Koulutustyyppi] = List(Amm, AmmTutkinnonOsa, Lk, Muu, Yo, Amk)
 }
