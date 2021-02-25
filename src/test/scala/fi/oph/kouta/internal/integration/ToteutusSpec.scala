5da9b7159ed2834540344ae82f88350eaa203fbb
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-02-25 15:18:49 +0200

    KTO-1141: Siirretty genereGetTestin testit toteutusSpeciin

--- src/test/scala/fi/oph/kouta/internal/integration/ToteutusSpec.scala
+++ src/test/scala/fi/oph/kouta/internal/integration/ToteutusSpec.scala
@@ -1,23 +1,16 @@
 package fi.oph.kouta.internal.integration
 
-import fi.oph.kouta.internal.domain.Toteutus
 import fi.oph.kouta.internal.domain.oid.{KoulutusOid, ToteutusOid}
 import fi.oph.kouta.internal.integration.fixture.{AccessControlSpec, KoulutusFixture, ToteutusFixture}
 import fi.oph.kouta.internal.security.Role
 
-class ToteutusSpec
-    extends ToteutusFixture
-    with KoulutusFixture
-    with AccessControlSpec
-    with GenericGetTests[Toteutus, ToteutusOid] {
+class ToteutusSpec extends ToteutusFixture with KoulutusFixture with AccessControlSpec {
 
-  override val roleEntities               = Seq(Role.Toteutus)
-  override val getPath: String            = ToteutusPath
-  override val entityName: String         = "toteutus"
-  override val existingId: ToteutusOid    = ToteutusOid("1.2.246.562.17.789")
-  override val nonExistingId: ToteutusOid = ToteutusOid("1.2.246.562.17.0")
+  override val roleEntities      = Seq(Role.Toteutus)
+  val existingId: ToteutusOid    = ToteutusOid("1.2.246.562.17.789")
+  val nonExistingId: ToteutusOid = ToteutusOid("1.2.246.562.17.0")
 
-  val koulutusOid = KoulutusOid("1.2.246.562.13.789")
+  val koulutusOid: KoulutusOid = KoulutusOid("1.2.246.562.13.789")
 
   override def beforeAll(): Unit = {
     super.beforeAll()
@@ -25,5 +18,21 @@ class ToteutusSpec
     addMockToteutus(existingId, ChildOid, koulutusOid)
   }
 
-  getTests()
+  "GET /:id" should s"get toteutus from elastic search" in {
+    get(existingId, defaultSessionId)
+  }
+
+  it should s"return 404 if toteutus not found" in {
+    get(s"$ToteutusPath/$nonExistingId", headers = Seq(defaultSessionHeader)) {
+      status should equal(404)
+      body should include(s"Didn't find id $nonExistingId")
+    }
+  }
+
+  it should "return 401 without a valid session" in {
+    get(s"$ToteutusPath/$nonExistingId") {
+      status should equal(401)
+      body should include("Unauthorized")
+    }
+  }
 }
