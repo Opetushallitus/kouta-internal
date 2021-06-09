4d10e03b25186261c3a8ad601a11558e1b105de6
August Kilponen <august.kilponen@gmail.com>
2021-06-09 18:17:41 +0300

    Korjattu hakukohteiden läpikäyntiä haun oikeuksien tarkistuksessa

--- src/main/scala/fi/oph/kouta/internal/elasticsearch/HakuClient.scala
+++ src/main/scala/fi/oph/kouta/internal/elasticsearch/HakuClient.scala
@@ -24,9 +24,8 @@ class HakuClient(val index: String, val client: ElasticClient)
   private def byTarjoajaAndTila(tarjoajaOids: Option[Set[OrganisaatioOid]], haku: HakuIndexed): Boolean =
     tarjoajaOids.fold(true)(oids =>
       haku.hakukohteet.exists(hakukohde => {
-        logger.info("Hakukohde: {}", hakukohde)
         hakukohde.tila match {
-          case Tallennettu => return false
+          case Tallennettu => false
           case _ =>
             hakukohde.jarjestyspaikka.fold(hakukohde.toteutus.tarjoajat.exists(t => oids.contains(t.oid)))(j =>
               oids.contains(j.oid)
