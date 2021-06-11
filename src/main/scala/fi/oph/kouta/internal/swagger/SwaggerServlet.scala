d3b718debea0ab0a005b25aa353fba9eabdc3650
August Kilponen <august.kilponen@gmail.com>
2021-06-11 19:09:00 +0300

    Päivitetty rajapintojen dokumentaatiota

--- src/main/scala/fi/oph/kouta/internal/swagger/SwaggerServlet.scala
+++ src/main/scala/fi/oph/kouta/internal/swagger/SwaggerServlet.scala
@@ -19,23 +19,28 @@ class SwaggerServlet extends ScalatraServlet {
         |info:
         |  title: kouta-internal
         |  description: >
-        |    Uuden koulutustarjonnan ulkoinen API.
+        |    Uuden koulutustarjonnan Opintopolun sisäisille palveluille tarkoitetut rajapinnat.
         |
         |
-        |    Ohjeet kirjautumiseen rajapintojen kutsujalle:
-        |    [https://confluence.csc.fi/display/oppija/Rajapintojen+autentikaatio](https://confluence.csc.fi/display/oppija/Rajapintojen+autentikaatio)
+        |    Ohjeita rajapintojen kutsujalle:
+        |
+        |
+        |    * [Autentikaatio](https://wiki.eduuni.fi/x/L95cCw)
+        |
+        |    * [Kutsujan tunniste (caller-id)](https://wiki.eduuni.fi/x/0MeKCg)
+        |
+        |    * [CSRF-suojaus](https://wiki.eduuni.fi/x/EL_hCw)
         |
         |
         |    Helpoin tapa kirjautua sisään Swagger-ui:n käyttäjälle on avata
         |    [/kouta-internal/auth/login](/kouta-internal/auth/login) uuteen selainikkunaan.
         |    Jos näkyviin tulee `{"personOid":"1.2.246.562.24.xxxx"}` on kirjautuminen onnistunut. Jos näkyviin tulee
         |    opintopolun kirjautumisikkuna, kirjaudu sisään.
-        |  version: 0.1-SNAPSHOT
         |  termsOfService: https://opintopolku.fi/wp/fi/opintopolku/tietoa-palvelusta/
         |  contact:
-        |    name: ""
+        |    name: "Opetushallitus"
         |    email: verkkotoimitus_opintopolku@oph.fi
-        |    url: ""
+        |    url: "https://www.oph.fi/"
         |  license:
         |    name: "EUPL 1.1 or latest approved by the European Commission"
         |    url: "http://www.osor.eu/eupl/"
@@ -63,7 +68,7 @@ class SwaggerServlet extends ScalatraServlet {
          |        type: string
          |        default: kouta-internal-swagger
          |      required: true
-         |      description: Kutsujan <a href="https://confluence.csc.fi/pages/viewpage.action?pageId=50858064">Caller ID</a>
+         |      description: Kutsujan [Caller ID](https://wiki.eduuni.fi/x/0MeKCg)
          |  schemas:
          |""".stripMargin
 
