94309492284f265ee4460f940f9accc6b4701340
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2021-08-31 08:45:26 +0300

    KTO-1341: Lisätty swagger-dokumentaatio

--- src/main/scala/fi/oph/kouta/internal/servlet/OdwServlet.scala
+++ src/main/scala/fi/oph/kouta/internal/servlet/OdwServlet.scala
@@ -2,6 +2,7 @@ package fi.oph.kouta.internal.servlet
 
 import fi.oph.kouta.internal.database.SessionDAO
 import fi.oph.kouta.internal.service.OdwService
+import fi.oph.kouta.internal.swagger.SwaggerPaths.registerPath
 import org.scalatra.FutureSupport
 
 import scala.concurrent.ExecutionContext
@@ -14,7 +15,24 @@ class OdwServlet(odwService: OdwService,  val sessionDAO: SessionDAO)
 
   override def executor: ExecutionContext = global
 
-  //registerPath("/odw/listHaut", "")
+  registerPath(
+    "/odw/listHaut",
+    """    get:
+    |        summary: Listaa julkaistut ja arkistoidut haut
+    |        description: Listaa kaikki julkaistut ja/tai arkistoidut haut
+    |        operationId: Listaa haut
+    |        tags:
+    |          - Odw
+    |        responses:
+    |          '200':
+    |            description: Ok
+    |            content:
+    |              application/json:
+    |                schema:
+    |                  type: array
+    |                  items:
+    |                    $ref: '#/components/schemas/Haku'
+    |""".stripMargin)
   get("/listHaut") {
     odwService.listAllHaut
   }
