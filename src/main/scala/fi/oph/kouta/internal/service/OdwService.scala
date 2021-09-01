450b4d84d3898ad2bfc8b045c60c203866b7c4b3
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2021-09-01 09:36:20 +0300

    KTO-1341: APIt hakuOidien listaukselle ja hakujen listaukselle oidien perusteella

--- src/main/scala/fi/oph/kouta/internal/service/OdwService.scala
+++ src/main/scala/fi/oph/kouta/internal/service/OdwService.scala
@@ -2,15 +2,18 @@ package fi.oph.kouta.internal.service
 
 import fi.oph.kouta.internal.domain.Haku
 import fi.oph.kouta.internal.domain.enums.Julkaisutila
+import fi.oph.kouta.internal.domain.oid.HakuOid
 import fi.oph.kouta.internal.elasticsearch.HakuClient
 import fi.oph.kouta.internal.security.Authenticated
 
 import scala.concurrent.Future
 
 class OdwService(hakuClient: HakuClient) {
-  def listAllHaut(offset: Int, limit: Option[Int])(implicit authenticated: Authenticated): Future[Seq[Haku]] =
-    hakuClient.hautByJulkaisutila(Some(Seq(Julkaisutila.Julkaistu, Julkaisutila.Arkistoitu)), offset, limit)
+  def listAllHakuOids(offset: Int, limit: Option[Int])(implicit authenticated: Authenticated): Future[Seq[HakuOid]] =
+    hakuClient.hakuOidsByJulkaisutila(Some(Seq(Julkaisutila.Julkaistu, Julkaisutila.Arkistoitu)), offset, limit)
 
+  def findByOids(hakuOids: Set[HakuOid])(implicit authenticated: Authenticated): Future[Seq[Haku]] =
+    hakuClient.findByOids(hakuOids)
 }
 
 object OdwService extends OdwService(HakuClient)
