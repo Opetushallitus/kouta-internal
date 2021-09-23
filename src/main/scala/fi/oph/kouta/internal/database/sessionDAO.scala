76cfea10c2d2bb60d28847a3df022947d022aba7
Mikko Siukola <mikko.siukola@cybercom.com>
2021-09-23 10:36:24 +0300

    OY-3094 Session length to 4 hours, no updates to timestamp

--- src/main/scala/fi/oph/kouta/internal/database/sessionDAO.scala
+++ src/main/scala/fi/oph/kouta/internal/database/sessionDAO.scala
@@ -90,18 +90,14 @@ class SessionDAO(db: KoutaDatabase) extends SQLHelpers {
       case None =>
         deleteSession(id).andThen(DBIO.successful(None))
       case Some(t) =>
-        updateLastRead(id).andThen(DBIO.successful(Some(t)))
+        DBIO.successful(Some(t))
     }
 
   private def getSessionQuery(id: UUID) =
     sql"""select cas_ticket, person from sessions
-          where id = $id and last_read > now() - interval '60 minutes'"""
+          where id = $id and last_read > now() - interval '4 hours'"""
       .as[(Option[String], String)]
       .headOption
-
-  private def updateLastRead(id: UUID) =
-    sqlu"""update sessions set last_read = now()
-           where id = $id and last_read < now() - interval '30 minutes'"""
 
   private def searchAuthoritiesBySession(sessionId: UUID) =
     sql"""select authority from authorities where session = $sessionId""".as[String]
