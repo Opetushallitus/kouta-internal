911923a6eb70135a3fc0f7af24ca0d8885dfc68f
August Kilponen <august.kilponen@gmail.com>
2021-10-14 12:03:06 +0300

    Lisää threadeja jettylle

--- src/main/scala/fi/oph/kouta/internal/JettyLauncher.scala
+++ src/main/scala/fi/oph/kouta/internal/JettyLauncher.scala
@@ -19,7 +19,7 @@ object JettyLauncher extends Logging {
 }
 
 class JettyLauncher(val port: Int) {
-  val threadPool: ThreadPool = new QueuedThreadPool(20, 5, 60000)
+  val threadPool: ThreadPool = new QueuedThreadPool(60, 10, 60000)
   val server                 = new Server(threadPool)
   val context                = new WebAppContext()
   context.setBaseResource(Resource.newClassPathResource("webapp"))
