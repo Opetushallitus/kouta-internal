34e6e5907322a6fa428dd24a665600890de59b6f
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-06-17 13:00:20 +0300

    error log on cached elastic query

--- src/main/scala/fi/oph/kouta/internal/elasticsearch/CachedElasticClient.scala
+++ src/main/scala/fi/oph/kouta/internal/elasticsearch/CachedElasticClient.scala
@@ -7,10 +7,13 @@ import com.sksamuel.elastic4s.requests.get.GetRequest
 import com.sksamuel.elastic4s.requests.searches.{SearchRequest, SearchScrollRequest}
 import com.sksamuel.elastic4s.{CommonRequestOptions, ElasticClient, Executor, Functor, Handler, Response}
 import fi.oph.kouta.internal.KoutaConfigurationFactory
+import org.slf4j.LoggerFactory
 
+import scala.concurrent.ExecutionContext.global
 import scala.concurrent.Future
 
 case class CachedElasticClient(client: ElasticClient) {
+  private lazy val logger = LoggerFactory.getLogger(getClass)
   private lazy val cache = Scaffeine()
     .expireAfterWrite(KoutaConfigurationFactory.configuration.elasticSearchConfiguration.cacheTimeoutSeconds.seconds)
     .buildAsync[Any, Any]()
@@ -22,7 +25,13 @@ case class CachedElasticClient(client: ElasticClient) {
       manifest: Manifest[U],
       options: CommonRequestOptions
   ): Future[Response[U]] = {
-    def executeForReal(): Future[Response[U]] = client.execute(t)
+    def executeForReal(): Future[Response[U]] = {
+      val f = client.execute(t)
+      f.failed.foreach(t => {
+        logger.error(s"ElasticSearch query failed: ${t.getMessage}", t)
+      })(global)
+      f
+    }
 
     t match {
       case get: GetRequest =>
