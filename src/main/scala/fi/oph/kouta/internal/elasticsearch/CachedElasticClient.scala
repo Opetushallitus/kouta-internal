96bb903bdff815e1a1622ad77feb89f16b7bbd42
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-09-05 14:32:50 +0300

    soft values

--- src/main/scala/fi/oph/kouta/internal/elasticsearch/CachedElasticClient.scala
+++ src/main/scala/fi/oph/kouta/internal/elasticsearch/CachedElasticClient.scala
@@ -13,11 +13,17 @@ import scala.concurrent.ExecutionContext.global
 import scala.concurrent.Future
 import boopickle.Default._
 
+object G {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
 case class CachedElasticClient(client: ElasticClient) extends Logging {
   private lazy val cache = Scaffeine()
     .softValues()
     .expireAfterWrite(KoutaConfigurationFactory.configuration.elasticSearchConfiguration.cacheTimeoutSeconds.seconds)
-    .build[Array[Byte], Future[Response[Any]]]()
+    .build[Any, Future[Response[Any]]]()
 
   def execute[T, U](t: T)(implicit
       executor: Executor[Future],
@@ -26,22 +32,24 @@ case class CachedElasticClient(client: ElasticClient) extends Logging {
       manifest: Manifest[U],
       options: CommonRequestOptions
   ): Future[Response[U]] = {
+    val key = t
     def executeForReal(): Future[Response[U]] = {
       val f = client.execute(t)
       f.failed.foreach(t => {
+        cache.invalidate(key)
         logger.error(s"ElasticSearch query failed: ${t.getMessage}", t)
       })(global)
       f
     }
-    def asArray(): Array[Byte] = Pickle.intoBytes(t).array()
+
 
     t match {
       case get: GetRequest =>
-        cache.get(asArray(), _ => executeForReal()).mapTo[Response[U]]
+        cache.get(key, _ => executeForReal()).mapTo[Response[U]]
       case search: SearchRequest =>
-        cache.get(asArray(), _ => executeForReal()).mapTo[Response[U]]
+        cache.get(key, _ => executeForReal()).mapTo[Response[U]]
       case search: SearchScrollRequest =>
-        cache.get(asArray(), _ => executeForReal()).mapTo[Response[U]]
+        cache.get(key, _ => executeForReal()).mapTo[Response[U]]
       case _ =>
         executeForReal()
     }
