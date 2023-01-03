d9b95a413e508b69468b7cc9f0481dfc251cf2b6
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-01-03 14:16:10 +0200

    OY-4050: Ei rekisteröidä swaggeriin samoja kenttiä moneen kertaan

--- src/main/scala/fi/oph/kouta/internal/swagger/SwaggerPaths.scala
+++ src/main/scala/fi/oph/kouta/internal/swagger/SwaggerPaths.scala
@@ -5,6 +5,6 @@ object SwaggerPaths {
   var paths: Map[String, List[String]] = Map[String, List[String]]()
 
   def registerPath(path: String, yaml: String): Unit =
-    paths += (path -> (paths.getOrElse(path, List[String]()) ++ List(yaml)))
+    paths += (path -> (paths.getOrElse(path, List[String]()) ++ List(yaml)).distinct)
 
 }
