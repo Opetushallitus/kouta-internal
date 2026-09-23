b2701a0d4d21ad28a06da0c36eeddbf2209aa678
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-09-23 09:00:44 +0300

    OY-5743: Korjataan swaggerin urlia

--- src/test/scala/fi/oph/kouta/internal/integration/SwaggerServletSpec.scala
+++ src/test/scala/fi/oph/kouta/internal/integration/SwaggerServletSpec.scala
@@ -7,9 +7,9 @@ class SwaggerServletSpec extends SwaggerFixture {
   "Swagger" should "have valid spec" in {
     get("/swagger/swagger.yaml") {
       val result  = new OpenAPIV3Parser().readContents(body, null, null)
-      val openApi = result.getOpenAPI()
+      val openApi = result.getOpenAPI
       openApi should not equal (null)     // Parsimisen pitää onnistua (on validia YML:ää)
-      result.getMessages() shouldBe empty // Ei virheitä tai varoituksia swaggerin parsinnasta
+      result.getMessages shouldBe empty // Ei virheitä tai varoituksia swaggerin parsinnasta
     }
   }
 }
