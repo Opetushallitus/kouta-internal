a83d4e91eb805a28796f69b185665d4bd2a60c20
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2026-09-23 11:39:10 +0300

    OY-5743: Korjataan GET hakukohde/findbyoids-rajapinnan swaggerin examplet

--- src/main/scala/fi/oph/kouta/internal/servlet/HakukohdeServlet.scala
+++ src/main/scala/fi/oph/kouta/internal/servlet/HakukohdeServlet.scala
@@ -156,7 +156,7 @@ class HakukohdeServlet(hakukohdeService: HakukohdeService, val sessionDAO: Sessi
       |              type: string
       |          required: false
       |          description: Organisaatio joka on hakukohteen tarjoaja
-      |          example: 1.2.246.562.10.00000000001,1.2.246.562.10.00000000002
+      |          example: [1.2.246.562.10.00000000001, 1.2.246.562.10.00000000002]
       |      requestBody:
       |          description: Palautettavien hakukohteiden oidit JSON-arrayna
       |          content:
@@ -165,8 +165,7 @@ class HakukohdeServlet(hakukohdeService: HakukohdeService, val sessionDAO: Sessi
       |                 type: array
       |                 items:
       |                   type: string
-      |                 example:
-      |                   array: ["1.2.246.562.10.00000000001","1.2.246.562.10.00000000002"]
+      |                 example: [1.2.246.562.10.00000000001, 1.2.246.562.10.00000000002]
       |      responses:
       |        '200':
       |          description: Ok
